436ee4708d50ca5b223e397d06429569599be351
Ramasubramani N <RAMASUBRAMANI_N@intuit.com>
2020-04-10 17:12:54 +0530

    Fixing Application Logs

--- src/main/java/com/psg/ramasubramanin/kafka/KafkaApplication.java
+++ src/main/java/com/psg/ramasubramanin/kafka/KafkaApplication.java
@@ -1,5 +1,7 @@
 package com.psg.ramasubramanin.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
@@ -11,9 +13,10 @@ import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class KafkaApplication 
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaApplication.class);
     public static void main( String[] args )
     {
         SpringApplication.run(KafkaApplication.class, args);
-        System.out.println("Kafka Application started...");
+        LOGGER.debug("Kafka Application started...");
     }
 }
